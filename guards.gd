36dc989a20c108cfb639731defb99b8f12cbb76c
Ryan Lay <30853424+not-fritos@users.noreply.github.com>
2026-01-31 20:25:59 -0600

    guards face the way they're moving (no walking anim)

--- guards.gd
+++ guards.gd
@@ -7,11 +7,19 @@ const GUARD_SIGHT_RANGE = 32
 @onready var player_check: RayCast2D = $player_check
 @onready var line_2d: Line2D = $player_check/Line2D
 @onready var player_character: CharacterBody2D = $"../PlayerCharacter"
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var _position_last_frame := Vector2()
+var _cardinal_direction = 0
 
 func _physics_process(delta: float) -> void:
 	var direction = pick_new_direction()
+	var facing_direction = get_facing_direction()
+	play_walking_animation(facing_direction)
+	
 	velocity = direction * SPEED * delta
 	move_and_slide()
+	
 	
 func pick_new_direction():
 	# track the player
@@ -44,3 +52,39 @@ func check_door():
 			
 			if collider.is_door(atlas_coords):
 				collider.open_door(map_coords)
+
+func get_facing_direction():
+	var motion = position - _position_last_frame
+
+	if motion.length() > 0.0001:
+		_cardinal_direction = int(4.0 * (motion.rotated(PI / 4.0).angle() + PI) / TAU)
+
+	_position_last_frame = position
+	
+	return _cardinal_direction
+
+enum IDLE {
+	LEFT = 0,
+	RIGHT = 3,
+	DOWN = 6,
+	UP = 9
+}
+
+enum WALK {
+	LEFT = 1,
+	RIGHT = 4,
+	DOWN = 7,
+	UP = 10,
+}
+	
+func play_walking_animation(cardinal_direction: int):
+	
+	match cardinal_direction:
+		0: # left
+				animated_sprite_2d.set_frame_and_progress(IDLE.LEFT, 0)
+		1: # up
+				animated_sprite_2d.set_frame_and_progress(IDLE.UP, 0)
+		2: # right
+				animated_sprite_2d.set_frame_and_progress(IDLE.RIGHT, 0)
+		3: # down
+			animated_sprite_2d.set_frame_and_progress(IDLE.RIGHT, 0)
